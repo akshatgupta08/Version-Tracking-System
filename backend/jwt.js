bbb432737dfb80b986ab0d3f3eed2119a999e811
akshatgupta08 <akshatvasu2005@gmail.com>
2024-08-05 11:18:42 +0530

    Delete redundant code

--- backend/jwt.js
+++ backend/jwt.js
@@ -2,28 +2,6 @@ import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 dotenv.config();
 
-/*const jwtAuthMiddleware = (req, res, next) => {
-
-    // first check request headers has authorization or not
-    const authorization = req.headers.authorization
-    if(!authorization) return res.status(401).json({ error: 'Token Not Found' });
-
-    // Extract the jwt token from the request headers
-    const token = req.headers.authorization.split(' ')[1];
-    if(!token) return res.status(401).json({ error: 'Unauthorized' });
-
-    try{
-        // Verify the JWT token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-       
-        next();
-    }catch(err){
-        console.error(err);
-        res.status(401).json({ error: 'Invalid token' });
-    }
-}*/
-
 /*The function checks if it received a valid JSON token from the front-end.*/
 const jwtAuthMiddle = (req,res) => {
 // first check request headers has authorization or not
